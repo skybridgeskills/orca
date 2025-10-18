172d96bb9cd0ab5d6869c538a244121673286676
Nate Otto <nate@ottonomy.net>
2025-10-18 13:44:28 -0700

    Show achievement edit capability on detail Fix erroneously displayed edit categories link

--- src/routes/achievements/+page.server.ts
+++ src/routes/achievements/+page.server.ts
@@ -3,9 +3,12 @@ import type { PageServerLoad } from './$types';
 import { canEditAchievements } from '$lib/server/permissions';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
+	
 	// Check if user has permission to edit achievements
 	let editAchievementCapability = false;
+	let editCategoriesCapability = false;
 	if (locals.session?.user?.id) {
+		editCategoriesCapability = (['GENERAL_ADMIN', 'CONTENT_ADMIN'].includes(locals.session.user.orgRole || 'none')) 
 		editAchievementCapability = await canEditAchievements({
 			user: {
 				id: locals.session.user.id,
@@ -37,6 +40,7 @@ export const load: PageServerLoad = async ({ params, locals }) => {
 
 	return {
 		editAchievementCapability,
+		editCategoriesCapability,
 		achievements,
 		categories
 	};
